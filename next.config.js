775b50fd0197576c02b743ce60e0a2184522b57a
PablodeMelo1 <pablodemelo14@gmail.com>
2025-08-21 14:50:18 -0300

    Add next.config.js 1755798618261

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  // Configurar el puerto por defecto
+  async rewrites() {
+    return []
+  },
+}
+
+module.exports = nextConfig
